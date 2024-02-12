51917d6329187942cd6aad25a9498ca326e7e115
Garrepalli Dhanush <35004425+Dhanush-Garrepalli@users.noreply.github.com>
2024-02-12 15:51:04 +0530

    Update json_validator.py

--- json_validator.py
+++ json_validator.py
@@ -12,15 +12,14 @@ if st.button('Validate and Format JSON'):
     except ValueError as e:
         st.error(f'Invalid JSON: {e}')
 
-# Ensure visitor_count is initialized at the start of the script
+'''
 if 'visitor_count' not in st.session_state:
     st.session_state['visitor_count'] = 1
 else:
-    # Increment the visitor count only when the button is pressed
+
     st.session_state['visitor_count'] += 1
 
-# Use markdown with HTML for centering at the bottom, safely accessing visitor_count
 footer = """<div style='text-align: center;'>
                 <p style='margin: 0; padding-bottom: 2rem;'>Visitor Count: {} | Dhanush Garrepalli</p>
             </div>""".format(st.session_state.get('visitor_count', 1))
-st.markdown(footer, unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)'''
