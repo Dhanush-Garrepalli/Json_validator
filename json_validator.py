c4717702cccc0d45906c738b57f5185bdf7d39c8
Garrepalli Dhanush <35004425+Dhanush-Garrepalli@users.noreply.github.com>
2024-02-12 15:53:05 +0530

    Update json_validator.py

--- json_validator.py
+++ json_validator.py
@@ -12,14 +12,3 @@ if st.button('Validate and Format JSON'):
     except ValueError as e:
         st.error(f'Invalid JSON: {e}')
 
-'''
-if 'visitor_count' not in st.session_state:
-    st.session_state['visitor_count'] = 1
-else:
-
-    st.session_state['visitor_count'] += 1
-
-footer = """<div style='text-align: center;'>
-                <p style='margin: 0; padding-bottom: 2rem;'>Visitor Count: {} | Dhanush Garrepalli</p>
-            </div>""".format(st.session_state.get('visitor_count', 1))
-st.markdown(footer, unsafe_allow_html=True)'''
